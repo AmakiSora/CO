5d54adef1d01ebafe47c92257d3981fcadb49e8b
NOCOSMOS <50519740+NOCOSMOS@users.noreply.github.com>
2022-11-07 00:10:31 +0800

    CO v1.0.2 引入ElementPlus

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import 'element-plus/dist/index.css'
 
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(ElementPlus, {
+    locale: zhCn,
+})
 app.mount('#app')
