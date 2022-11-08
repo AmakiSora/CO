470400ccf3abeefaebfa8539520f79d8e923f84c
NOCOSMOS <50519740+NOCOSMOS@users.noreply.github.com>
2022-11-09 00:57:45 +0800

    CO v1.0.5 分离组件

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,15 @@
-<script setup>
+<script>
 import {RouterLink, RouterView} from 'vue-router'
 import {ref} from 'vue'
-
-const input = ref('233')
-console.log(input.value)
+import MainInput from "./components/MainInput.vue";
+export default {
+  components: {MainInput}
+}
 </script>
 
 <template>
-  <div class="main-input-dev">
-    <el-input
-        v-model="input"
-        placeholder="Please input"
-        size="large"
-        class="main-input"
-    >
-      <template #prepend>
-        <el-button>
-          <el-icon>
-            <Search/>
-          </el-icon>
-        </el-button>
-      </template>
-    </el-input>
-  </div>
+  <MainInput></MainInput>
 </template>
 
 <style scoped>
-.main-input {
-  text-align: center;
-  width: 780px;
-}
 </style>
---
+++ src/components/MainInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="main-input-dev">
+    <el-input
+        v-model="input"
+        placeholder=""
+        size="large"
+        class="main-input"
+        @input="inputChange"
+    >
+      <template #prepend>
+        <el-button>
+          <el-icon>
+            <Search/>
+          </el-icon>
+        </el-button>
+      </template>
+    </el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainInput",
+  data() {
+    return {
+      input: ""
+    }
+  },
+  methods: {
+    inputChange(){
+      console.log(2333)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.main-input {
+  text-align: center;
+  width: 780px;
+}
+</style>
